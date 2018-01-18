b5ab5236a8e5cabcfb482864cf8b1074a6ca8aa9
Vuyisile Ndlovu <terrameijar@gmail.com>
2018-01-18 11:41:36 +0200

    Refactored GUI code

--- toils_gui.py
+++ toils_gui.py
@@ -2,28 +2,27 @@ import datetime
 import time
 import gi
 import pdb
+import logging
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib
-
 
 
 class TimeTracker(Gtk.Window):
 
     def __init__(self):
 
-
         self.builder = Gtk.Builder()
         self.builder.add_from_file("layout1.glade")
-
         self.window = self.builder.get_object("window2")
-        #self.client_info_window = self.builder.get_object("window_client_info")
-        #self.client_info_window.show_all()
-
         self.window.set_title("Toils App")
+        self.window.connect("delete-event", Gtk.main_quit)
+        self.window.show_all()
+
+        # GUI Widgets
         self.btn_start = self.builder.get_object("btn_start")
         self.btn_stop = self.builder.get_object("btn_stop")
-        self.label4 = self.builder.get_object("label4")
+        self.lbl_time = self.builder.get_object("lbl_time")
         self.lbl_task_name = self.builder.get_object("lbl_taskName")
         self.entry_task_name = self.builder.get_object("entry_activity")
 
@@ -31,50 +30,37 @@ class TimeTracker(Gtk.Window):
         self.main_menubar = self.builder.get_object("menubar2")
 
         # Main menu items
-        self.file_menu = self.builder.get_object("menuitem5")
-        self.edit_menu = self.builder.get_object("menuitem6")
+        self.file_menu = self.builder.get_object("menuitem_file")
+        self.edit_menu = self.builder.get_object("menuitem_edit")
 
         # Drop Down Menu items
-        self.file_new = self.builder.get_object("imagemenuitem11")
-        edit_prefs = self.builder.get_object("imagemenuitem16")
+        self.file_new = self.builder.get_object("menuitem_file_new")
+        self.edit_prefs = self.builder.get_object("menuitem_edit_pref")
 
-        # Connect menu items to signals
-        # self.file_new.connect("activate", self.stub)
-        self.handler_id = self.file_new.connect("activate", self.stub)
+        # Widget-Signal connections
         self.btn_start.connect("clicked", self.start_timer)
         self.btn_stop.connect("clicked", self.stop_timer)
+        self.file_new.connect("activate", self.new_client_window_open)
 
-        # Client Info window controls
-        #self.btn_cancel = self.builder.get_object("btn_cancel")
-        #self.btn_cancel.connect("clicked", self.stub_window_close)
-
-
+        # Other initialisations
         self.timer_active = False
-        # should this be here?
-        self.window.connect("delete-event", Gtk.main_quit)
-        self.window.show_all()
 
 
     def save_button_pressed(self, widget):
-
-        print(self.client_name.get_text())
+        logging.debug(self.client_name.get_text())
+        # TODO: Save client in a DB
         fobj = open('time_data.txt', 'a+')
         fobj.write(self.client_name.get_text())
         fobj.write('\n')
         fobj.close()
 
 
-    def stub_window_close(self, widget):
-        #pass
-        #self.client_info_window.disconnect(self.handler_id)
+    def client_info_window_close(self, widget):
         self.client_info_window.close()
 
-
-    def stub(self, widget):
+    def new_client_window_open(self, widget):
         # Draws the Client Information Window
-        print("Stub function activated")
-
-        #self.client_info_window = self.builder.get_object("window_client_info")
+        logging.debug("new_client_window_open function activated")
         #pdb.set_trace()
         self.builder.add_from_file("layout1.glade")
         self.client_info_window = self.builder.get_object("window_client_info")
@@ -83,13 +69,9 @@ class TimeTracker(Gtk.Window):
         self.client_name = self.builder.get_object("entry_client_name")
         self.btn_cancel = self.builder.get_object("btn_cancel")
         self.btn_save = self.builder.get_object("btn_save")
-        self.btn_cancel.connect("clicked", self.stub_window_close)
+        self.btn_cancel.connect("clicked", self.client_info_window_close)
         self.btn_save.connect("clicked", self.save_button_pressed)
         self.client_info_window.show_all()
-        #client_info_window.show_all()
-        #print(self.list_toplevels())
-        #for item in dir(self.client_info_window):
-        #    print(item)
 
     def time_function(self, time_val):
         '''Main Timer function'''
@@ -120,8 +102,8 @@ class TimeTracker(Gtk.Window):
         time_delta = int(time.time() - start_time)
         self.lbl_task_name.set_text(self.entry_task_name.get_text())
         if self.timer_active:
-            #self.label4.set_text(str(time_delta))
-            self.label4.set_text(self.time_function(time_delta))
+
+            self.lbl_time.set_text(self.time_function(time_delta))
             return True
         else:
             return False
@@ -146,14 +128,7 @@ class TimeTracker(Gtk.Window):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)s - %(message)s')
     app = TimeTracker()
-    #app.connect("delete-event", Gtk.main_quit)
-    #app.show_all()
-    builder = Gtk.Builder()
-    builder.add_from_file("layout1.glade")
-    window = builder.get_object("window3")
-    button9 = builder.get_object("button9")
-    label4 = builder.get_object("label4")
     Gtk.main()
-
-# TODO: Update builder refs in the__init__ and __main__
